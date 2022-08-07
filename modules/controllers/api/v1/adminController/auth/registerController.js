4e9c8b6f1d8acd064e948f23ac5b009b836e72cf
alireza <alirezakhd2705@gmail.com>
2022-08-07 23:20:27 +0430

    implementing register admin routes

--- modules/controllers/api/v1/adminController/auth/registerController.js
+++ modules/controllers/api/v1/adminController/auth/registerController.js
@@ -1,30 +1,45 @@
 const InitializeController = require("./initializeController");
-const {validationResult} = require('express-validator');
+const {
+  validationResult
+} = require('express-validator/check');
 const config = require('../../../../../../config');
 const checkValidationErr = require(`${config.path.helper}/checkValidationErr`);
 
 module.exports = new(class registerController extends InitializeController {
   async register(req, res) {
-
-    //check validation
-    checkValidationErr(req , res , this)
     try {
-      const superAdmin = await this.model.User.findOne({
-        email: req.body.email
-      }).exec();
-      if (superAdmin) return this.abort(res, 422, logcode, "ایمل وارد شده تکراریست", null, "email");
+      const {
+        firstName,
+        lastName,
+        email,
+        username,
+        password,
+        mobile,
+        type
+      } = req.body;
+      //check validation
+      let errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return this.showValidationErrors(res, errors.array())
+      }
+      const admin = await this.model.User.findOne({
+        username
+      });
+      if (admin) return this.abort(res, 422, null, "ایمل وارد شده تکراریست", null, "email");
       const values = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        password: req.body.password,
-        type: "superAdmin",
+        firstName,
+        lastName,
+        username,
+        email,
+        mobile,
+        password,
+        type
       };
-      await this.model.User.create(values);
-      return this.ok(res, logcode, "با موفقیت اضافه شد");
+      let result = await this.model.User.create(values);
+      return this.helper.response(res, "با موفقیت اضافه شد", null, 200, result);
     } catch (err) {
       console.log(err);
-      return this.abort(res, 500, logcode);
+      return this.abort(res, 500, null);
     }
   }
 })();
