6c6c84281250b9a8fcfedc050a7a164fb00f721e
alireza <alirezakhd2705@gmail.com>
2022-08-09 23:29:07 +0430

    implement single file for user but still working on it

---
+++ modules/controllers/api/v1/userController/file/singleController.js
@@ -0,0 +1,43 @@
+const InitializeController = require("./initializeController");
+
+
+module.exports = new(class singleController extends InitializeController {
+    async single(req, res) {
+        try {
+            let slug = req.params.slug;
+
+            if (!slug) return this.abort(res, null, 400, "slug param must be entered");
+
+            let file = await this.model.File.findOne({
+                slug
+            })
+            console.log(file);
+            if (!file)
+                return this.abort(res, null, 404, "file does not exist");
+
+            const response_key = req.body["g-recaptcha-response"];
+            console.log(response_key);
+            let response = await this.helper.recaptcha(response_key , req.connection.remoteAddress)    
+            let data = await response.json()
+            if(!data.success ){
+                return this.abort(res , null , 429 , {
+                    success: false ,
+                    message : "failed captcha verification"
+                })
+            }
+            //check private file and check password
+            if (file.isPrivate) {
+                let password = req.body.password;
+                if(!password || password !== file.password){
+                    return this.abort(res, null, 400, "password does not match");
+                }    
+            }
+            //access to file
+            const Transform = await this.helper.transform(file, this.helper.itemTransform, false);
+            return this.helper.response(res, "get file successfully", null , 200, Transform);
+        } catch (err) {
+            console.log(err);
+            return this.abort(res, 500, null);
+        }
+    }
+})();
