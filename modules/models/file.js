a347c0826e53ced80e6523f274bfcb8ee48a9a0f
alireza <alirezakhd2705@gmail.com>
2022-08-09 12:37:59 +0430

    renaming file and otp model

--- modules/models/file.js
+++ modules/models/file.js
@@ -3,11 +3,13 @@ const Schema = mongoose.Schema
 const timestamps = require('mongoose-timestamp')
 const aggregatePaginate = require('mongoose-aggregate-paginate-v2')
 
+
 const FileSchema = new Schema({
-  title: { type: String, required: true },
-  text: { type: String, required: true },
-  format: { type: String , default : 'txt' },
-  isPrivate : {type : Boolean , required : true , default : false},
+  slug : {type : String ,unique : true},
+  title: { type: String, required: true , trim : true  },
+  text: { type: String, required: true  },
+  format: { type: String , default : 'txt' , trim : true},
+  isPrivate : {type : Boolean , required : true , default : false,  trim : true},
   password: { type: String , default : '' },
   expired: { type: Date},
   userId: { type: Schema.Types.ObjectId, ref: "User" }
@@ -15,6 +17,7 @@ const FileSchema = new Schema({
 //
 FileSchema.plugin(timestamps)
 FileSchema.plugin(aggregatePaginate)
+
 
 //
 module.exports = mongoose.model('File', FileSchema);
